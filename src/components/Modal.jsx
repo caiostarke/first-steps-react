5e9368dbb9424d11bf66cb677a12fabdcb8825a6
crowlke <caiostarke@hotmail.com>
2024-03-13 10:59:13 -0300

    add inline css

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import './Modal.css'
 export default function Modal({children, handleClose}) {
   return (
     <div className="modal-backdrop">
-        <div className="modal"> 
+        <div className="modal" style={{border: "4px solid", borderColor: "#ff4500"}}>  
           { children }
           <button onClick={handleClose}> Close </button>
         </div>
